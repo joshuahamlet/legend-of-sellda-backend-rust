bb53c0940096408ae9eb18b5d70f0b0251e6cf47
Joshua Hamlet <jhamlet@tuta.io>
2023-02-21 18:47:39 -0800

    first commit

---
+++ src/models/user_model.rs
@@ -0,0 +1,13 @@
+use mongodb::bson::oid::ObjectId;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)] 
+pub struct User {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<ObjectId>,
+    pub name: String,
+    pub email: String, 
+    pub password: String,
+    pub date: Date,
+    is_admin: bool
+}
